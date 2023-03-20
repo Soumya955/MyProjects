c9e4dbe25141861645ee86b67253228693403ec9
soumya995 <soumyarb292@gmail.com>
2023-03-20 22:39:40 +0530

    added

--- experiment/index.js
+++ experiment/index.js
@@ -1,7 +1,21 @@
 
-;
-console.log(a)
 
-let a=0;
+
 var b=12;
+console.log(b)
+function a(){
+     b=13;
+    console.log(b)
+    function c(){
+         b=14;
+        console.log(b)
+        function d(){
+            b=15;
+            console.log(b)
+        }
+        d()
+    }
+    c()
+}
+a()
 
