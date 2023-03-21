e50651b428d02391168117e543e674a886d9b7e4
soumya995 <soumyarb292@gmail.com>
2023-03-21 20:29:59 +0530

    added

--- experiment/index.js
+++ experiment/index.js
@@ -1,12 +1,23 @@
 
 
-function a(){
-   var  b=13;
-    function c(){
-        console.log(++b)
-    }
-    c()
-    c()
+// function a(){
+//    var  b=13;
+//     function c(){
+//         console.log(++b)
+//     }
+//     c()
+//     c()
+// }
+// a()
+
+
+let N=4;
+let arr=[];
+for(let i=0;i<N;i++){
+    let arr1=new Array(N*2).fill("");
+    arr1[i]="\\";
+    arr1[(N*2)-1-i]="/"
+    console.log(arr1.join(" "))
 }
-a()
+
 
