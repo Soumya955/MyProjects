38790432cd0270ff479a8069629f1aaed8e2879e
soumya995 <soumyarb292@gmail.com>
2023-04-04 23:26:45 +0530

    added

--- tic-tac-toe/src/Cookies/CreateCookie.js
+++ tic-tac-toe/src/Cookies/CreateCookie.js
@@ -2,8 +2,9 @@ export function createRandomCookie() {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
     let cookieValue = '';
   
-
     
+
+
     // Generate a random string of 16 characters
     for (let i = 0; i < 16; i++) {
       cookieValue += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
