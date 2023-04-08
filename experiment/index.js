5352a3d953353a6dacadb47b39e035eb93de7ae4
soumya995 <soumyarb292@gmail.com>
2023-04-08 11:49:50 +0530

    added

--- experiment/index.js
+++ experiment/index.js
@@ -1,28 +1,38 @@
-//console.log(x)
-//const x=2;
-a()
-b()
-console.log(x)
-function a(){
-    // console.log(x)
-     const x=10;
-    const y=23
-    const ab=()=>{
-        sr()
-        function sr(){
-            console.log("hello",x)
-        }
-    }
-    // console.log(x)
-    c()
-    function c(){
-        // const x=1000;
-        ab()
-        //console.log(x,y)
-    }
-}
-function b(){
-    const x=100;
-    console.log(x)
-}
+// //console.log(x)
+// //const x=2;
+// a()
+// b()
+// console.log(x)
+// function a(){
+//     // console.log(x)
+//      const x=10;
+//     const y=23
+//     const ab=()=>{
+//         sr()
+//         function sr(){
+//             console.log("hello",x)
+//         }
+//     }
+//     // console.log(x)
+//     c()
+//     function c(){
+//         // const x=1000;
+//         ab()
+//         //console.log(x,y)
+//     }
+// }
+// function b(){
+//     const x=100;
+//     console.log(x)
+// }
 
+let sum=(a)=>{
+    return (b)=>{
+      if(b){
+        return sum(a+b);
+      }else{
+        return a
+      }
+    }
+}
+console.log(sum(2)(3)(5)())
