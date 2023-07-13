d05bdaf466eaf0fcbf69a4de8f2b79aa8fa94499
soumya995 <soumyarb292@gmail.com>
2023-07-13 23:05:47 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,27 +1,5 @@
-
-function soumya(input){
-  console.log(input)
-let object={}
-for(let i=0;i<input.length;i++){
-object[input[i]]=object[input[i]]?object[input[i]]+1:1;
+var y = 1;
+if (function f() {}) {
+  y += typeof f;
 }
-
-let max=-Infinity;
-for(let  key in object){
-if(object[key]>max){
-max=object[key];
-}
-}
-
-console.log(object,max)
-
-let ans="";
-for(let  key in object){
-if(object[key]==max){
-ans+=key;
-}
-}
-console.log(ans)
-}
-
-console.log(soumya("bananan"))
+console.log(y);
