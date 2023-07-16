defa613730299f659aa0e9c94ac8b116c107190a
soumya995 <soumyarb292@gmail.com>
2023-07-16 11:29:57 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -5,16 +5,8 @@ const fetchData = () => {
     }, 2000);
   });
 };
-const fetchData1 = (data) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Data fetched successfully!'+data);
-    }, 2000);
-  });
-};
+
 
 fetchData()
 .then(res=>console.log(res))
-fetchData1("OKKK")
-.then(res=>console.log(res))
-.catch(err=>console.log(err))
+
