12cccbc98637c9f7162942ecc841114c214ef112
soumya995 <soumyarb292@gmail.com>
2023-07-16 11:33:02 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,12 +1,13 @@
 const fetchData = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('Data fetched successfully!');
+      resolve("Data fetched successfully!");
     }, 2000);
   });
 };
 
-
-fetchData()
-.then(res=>console.log(res))
-
+const soumya = async () => { 
+  const data = await fetchData()
+  console.log(data)
+};
+soumya()
