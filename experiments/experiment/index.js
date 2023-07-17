1c1c30028965d2ebc1099a03163c913f31394131
soumya995 <soumyarb292@gmail.com>
2023-07-17 22:20:04 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,13 +1,20 @@
-const fetchData = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Data fetched successfully!");
-    }, 2000);
-  });
-};
+function maximumPairSum(cards) {
+  // Sort the cards in ascending order
+  cards.sort((a, b) => a - b);
 
-const soumya = async () => { 
-  const data = await fetchData()
-  console.log(data)
-};
-soumya()
+  let maxSum = 0;
+
+  // Pair up the cards by selecting the smallest and largest values
+  for (let i = 0; i < cards.length / 2; i++) {
+    const pairSum = cards[i] + cards[cards.length - 1 - i];
+    maxSum = Math.max(maxSum, pairSum);
+  }
+
+  return maxSum;
+}
+
+
+// Example usage:
+const cards = [1, 4, 3, 2];
+const maxPairSum = maximumPairSum(cards);
+console.log(maxPairSum);
