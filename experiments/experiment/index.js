e95c04c5915942a4ad76544e7cceac8c04448546
soumya995 <soumyarb292@gmail.com>
2023-07-18 11:16:36 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,9 +1,5 @@
 
 
-var array1 = [];
-var array2 = [];
-var areEqual = array1.toString() === array2.toString();
-console.log(areEqual); // true
 
 
-console.log(array1.toString(),array2.toString())
+console.log(NaN === NaN)
