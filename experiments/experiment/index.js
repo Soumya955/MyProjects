f1c35cc420c9544a12b19145a9401f5623f811af
soumya995 <soumyarb292@gmail.com>
2023-07-18 11:10:59 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,1 +1,9 @@
-console.log([]=[])
+
+
+var array1 = [1, 2, 3];
+var array2 = [1, 2, 3];
+var areEqual = array1.toString() === array2.toString();
+console.log(areEqual); // true
+
+
+console.log(array1.toString(),array2.toString())
