625d7df14215ecea3e39950f72ea90f68291d6a0
soumya995 <soumyarb292@gmail.com>
2023-07-24 19:21:00 +0530

    added

--- experiments/experiment-on-react/src/App.js
+++ experiments/experiment-on-react/src/App.js
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const apiUrl = 'http://localhost:8080/api/'
+fetch(apiUrl)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return res.json();
+  })
+  .then(data => {
+    console.log('Data from the API:', data);
+  })
+  .catch(err=> {
+    console.error('Fetch error:', err);
+  });
+
+   
   return (
     <div className="App">
       <header className="App-header">
