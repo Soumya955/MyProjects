ff3930151e4136e98180e1700fd8452d5a783812
soumya995 <soumyarb292@gmail.com>
2023-08-12 12:23:19 +0530

    added

--- experiments/experiment/index.js
+++ experiments/experiment/index.js
@@ -1,3 +1,17 @@
-const a=1;
-const a=2;
-console.log(a)
+function Counter() {
+    var count = 0;
+    let incrementCounter = function() { //anonymous function
+      count++;
+      console.log(count);
+    }
+    let decrementCounter = function() {
+      count--;
+      console.log(count);
+    }
+    return {incrementCounter,decrementCounter}
+  }
+  
+  var counter1 = new Counter();  // new keyword for constructor fun
+  counter1.incrementCounter();
+  counter1.incrementCounter();
+  counter1.decrementCounter();
