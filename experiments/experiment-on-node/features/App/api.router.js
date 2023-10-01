75625b6ecb17b07697c1298d0a0585c27f83bc24
soumya995 <soumyarb292@gmail.com>
2023-10-01 23:44:15 +0530

    added

--- experiments/experiment-on-node/features/App/api.router.js
+++ experiments/experiment-on-node/features/App/api.router.js
@@ -4,12 +4,10 @@ const axios = require('axios');
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  const apiUrl =
-    "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9564672,77.6634368&rankby=distance&key=AIzaSyAaCrELm-SemYV2KoduK7ESnZ2UmN44EeQ";
-
+  const apiUrl =`https://newsapi.org/v2/top-headlines?country=in&apiKey=2e41d48fbf244e48ba1db6e27c41834a`;
     axios.get(apiUrl)
     .then((data) => {
-     res.send(data.data.results);
+     res.send(data.data.articles);
     })
     .catch((err) => {
       console.error("Fetch error:", err);
