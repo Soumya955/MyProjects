26a17c6f9a1a3e9d30d67c355f69efc165903b00
soumya995 <soumyarb292@gmail.com>
2023-12-20 19:09:49 +0530

    added

--- experiments/experiment-on-javascript/index.js
+++ experiments/experiment-on-javascript/index.js
@@ -1,2 +1,17 @@
-let result = 10 * 'apple';
-console.log(result);
+function singleNumber(nums) {
+    let singleNumber = 0;
+  
+    for (let num of nums) {
+        console.log(singleNumber,num,"----")
+      singleNumber ^= num; 
+      console.log(singleNumber)// XOR each element with the accumulated result
+    }
+  
+    return singleNumber; // The final value in singleNumber is the unique element
+  }
+  
+  // Example usage
+  const nums = [2,1, 2, 3, 3];
+  const singleElement = singleNumber(nums);
+  console.log(`The single element is: ${singleElement}`); // Output: 1
+  
